2f92b911a035afe48e9bbc50e89a10e44ba002e3
Thomas Steiner <tomac@google.com>
2021-06-30 15:50:47 +0200

    Polishing

--- demo/script.js
+++ demo/script.js
@@ -1,9 +1,16 @@
 import '../src/index.js';
 
-document.querySelector('input').addEventListener('input', (e) => {
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+
+input.addEventListener('input', (e) => {
   e.target.value = new Intl.InputMask('credit-card-number').format(
     e.target.value,
   );
+});
+
+button.addEventListener('click', () => {
+  input.value = '';
 });
 
 document.querySelectorAll('.credit-card-number').forEach((td) => {
