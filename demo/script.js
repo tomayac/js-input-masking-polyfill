6f50f0d5a55fcd64f92e4a932ea5cc82b9f01706
Thomas Steiner <tomac@google.com>
2021-06-30 17:15:05 +0200

    Restore caret position

--- demo/script.js
+++ demo/script.js
@@ -4,9 +4,19 @@ const input = document.querySelector('input');
 const button = document.querySelector('button');
 
 input.addEventListener('input', (e) => {
+  // Save the caret position.
+  let caretPos = input.selectionStart;
+  const value = e.target.value;
+  // Apply the input masking.
   e.target.value = new Intl.InputMask('credit-card-number').format(
     e.target.value,
   );
+  // Restore the caret position while neutralizing the masking.
+  if (value !== e.target.value) {
+    caretPos += e.target.value.length - value.length;
+  }
+  input.selectionStart = caretPos;
+  input.selectionEnd = caretPos;
 });
 
 button.addEventListener('click', () => {
